f45496da699808ab056fc09677bc84f8f27efdd2
Jorge Luis Garnica <jorgegarba@gmail.com>
2022-08-04 18:58:04 -0500

    deleting old version

--- sequelize/seeders/buildings.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.buildings = void 0;
-exports.buildings = [
-    { id: 1, name: 'Proyecto Apollo' },
-    { id: 2, name: 'Proyecto Victoria' }
-];
