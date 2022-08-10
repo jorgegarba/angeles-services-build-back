1a6bdf5a84d833672c60c78f59c2d111fee5d4bd
Jorge Luis Garnica Blanco <jorgegarba@gmail.com>
2022-08-10 18:04:41 -0500

    uploading new version

---
+++ sequelize/seeders/typologies.js
@@ -0,0 +1,47 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.typologies = void 0;
+exports.typologies = [
+    {
+        id: 1,
+        name: 'FLAT TIPO 2',
+        area: 83,
+        building_id: 1,
+        url: 'https://firebasestorage.googleapis.com/v0/b/codigo9-166dd.appspot.com/o/angeles-photos%2FApolo-2.jpeg?alt=media&token=add68fcf-b6a0-4c75-b335-82a567b4b762'
+    },
+    {
+        id: 2,
+        name: 'FLAT TIPO 1-B',
+        area: 66,
+        building_id: 1,
+        url: 'https://firebasestorage.googleapis.com/v0/b/codigo9-166dd.appspot.com/o/angeles-photos%2FApolo-1-B.jpeg?alt=media&token=131a5d98-08ce-4730-b3cb-d8cb697b77b3'
+    },
+    {
+        id: 3,
+        name: 'FLAT TIPO 1-A',
+        area: 71,
+        building_id: 1,
+        url: 'https://firebasestorage.googleapis.com/v0/b/codigo9-166dd.appspot.com/o/angeles-photos%2FAPolo-1-A.jpeg?alt=media&token=6be16ee6-37b2-4064-9214-033ff03b36f8'
+    },
+    {
+        id: 4,
+        name: 'DUPLEX TIPO 3-A',
+        area: 133,
+        building_id: 1,
+        url: 'https://firebasestorage.googleapis.com/v0/b/codigo9-166dd.appspot.com/o/angeles-photos%2FApolo-3-A.jpeg?alt=media&token=543c2a17-e790-40ae-8eda-771a32018cce'
+    },
+    {
+        id: 5,
+        name: 'DUPLEX TIPO 3-B',
+        area: 103,
+        building_id: 1,
+        url: 'https://firebasestorage.googleapis.com/v0/b/codigo9-166dd.appspot.com/o/angeles-photos%2FApolo-3-B.jpeg?alt=media&token=f7473877-4a00-4caf-8d26-d22e96c90be7'
+    },
+    {
+        id: 6,
+        name: 'FLAT TIPO 1',
+        area: 101,
+        building_id: 2,
+        url: 'https://firebasestorage.googleapis.com/v0/b/codigo9-166dd.appspot.com/o/angeles-photos%2FVictoria%20-%202.jpeg?alt=media&token=7e00b233-0817-4594-ad86-d288580a5d05'
+    }
+];
