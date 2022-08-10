df50985362ea617b8c7cc321645b244199b7f528
Jorge Luis Garnica Blanco <jorgegarba@gmail.com>
2022-08-10 18:05:50 -0500

    uploading new version

---
+++ sequelize/seeders/users.js
@@ -0,0 +1,54 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.users = void 0;
+exports.users = [
+    {
+        id: 1,
+        fullname: 'Jorge Garnica',
+        type: 'vendedor',
+        phone: '974204853',
+        password: ''
+    },
+    {
+        id: 2,
+        fullname: 'Trinidad Canazas',
+        type: 'vendedor',
+        phone: '949364885',
+        password: ''
+    },
+    {
+        id: 3,
+        fullname: 'Nazareth Canazas',
+        type: 'vendedor',
+        phone: '977132989',
+        password: ''
+    },
+    {
+        id: 4,
+        fullname: 'Sofía Zevallos',
+        type: 'vendedor',
+        phone: '977114284',
+        password: ''
+    },
+    {
+        id: 5,
+        fullname: 'Enrique Garnica',
+        type: 'vendedor',
+        phone: '974204852',
+        password: ''
+    },
+    {
+        id: 6,
+        fullname: 'Ryldo Quispe',
+        type: 'vendedor',
+        phone: '993043566',
+        password: ''
+    },
+    {
+        id: 7,
+        fullname: 'Mylady Barrios',
+        type: 'vendedor',
+        phone: '980831147',
+        password: ''
+    }
+];
