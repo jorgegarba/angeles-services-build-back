91765a3be8936ffb364ebeeb5f52143722b2d898
Jorge Luis Garnica <jorgegarba@gmail.com>
2022-08-20 12:08:17 -0500

    deleting old version

--- sequelize/seeders/services.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.services = void 0;
-exports.services = [
-    { id: 1, name: 'Ascensor' },
-    { id: 2, name: 'Cochera eléctrica' },
-    { id: 3, name: 'Iluminación común' },
-];
