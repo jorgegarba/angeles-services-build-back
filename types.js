d877a3273f62d8679287aa56b3ef6764427f518d
Jorge Luis Garnica <jorgegarba@gmail.com>
2022-08-21 13:03:19 -0500

    new version

---
+++ types.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.exludedAttr = void 0;
+exports.exludedAttr = {
+    attributes: {
+        exclude: ['createdAt', 'updatedAt']
+    }
+};
