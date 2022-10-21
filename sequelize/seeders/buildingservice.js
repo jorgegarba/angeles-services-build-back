323913a4ee43cd7f9649252e11bb1f43b9616db3
Jorge Garnica <jorgegarba@gmail.com>
2022-10-20 22:36:40 -0500

    deleting old version
    Signed-off-by: Jorge Garnica <jorgegarba@gmail.com>

--- sequelize/seeders/buildingservice.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.buildingservice = void 0;
-exports.buildingservice = [
-    { building_id: 1, service_id: 1 },
-    { building_id: 1, service_id: 2 },
-    { building_id: 2, service_id: 1 },
-    { building_id: 2, service_id: 2 }
-];
--- sequelize/seeders/dptoservice.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.dptoservice = void 0;
-exports.dptoservice = [
-    { dpto_id: 1, service_id: 1 },
-    { dpto_id: 1, service_id: 2 },
-    { dpto_id: 2, service_id: 1 },
-    { dpto_id: 2, service_id: 2 }
-];
