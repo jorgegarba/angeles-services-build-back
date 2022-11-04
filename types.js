6c8283995be72ec084f9348f7f93c5b307d4e05c
Jorge Garnica <jorgegarba@gmail.com>
2022-11-04 00:25:30 -0500

    deleting old version
    Signed-off-by: Jorge Garnica <jorgegarba@gmail.com>

--- types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.exludedAttr = void 0;
-exports.exludedAttr = {
-    attributes: {
-        exclude: ['createdAt', 'updatedAt'],
-    },
-};
