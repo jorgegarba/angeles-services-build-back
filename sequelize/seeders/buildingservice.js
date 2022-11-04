6709aba6bee0840afa6165725bc9abb3fe926813
Jorge Garnica <jorgegarba@gmail.com>
2022-11-04 00:26:14 -0500

    new version
    Signed-off-by: Jorge Garnica <jorgegarba@gmail.com>

---
+++ sequelize/seeders/buildingservice.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.buildingservice = void 0;
+exports.buildingservice = [
+    { building_id: 1, service_id: 1 },
+    { building_id: 1, service_id: 2 },
+    { building_id: 2, service_id: 1 },
+    { building_id: 2, service_id: 2 },
+];
